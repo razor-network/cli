8173bfca278798c39ea33de12e4543e1d53ba96c
Hrishikesh Huilgolkar <hrishikesh911@gmail.com>
2019-11-04 21:02:06 +0530

    redeploy

--- misc/deploy.sh
+++ misc/deploy.sh
@@ -12,3 +12,5 @@ pm2 -f start index.js -- v 0x21D7ACbcAEa5dD43e28c41b37A1296d6aAa4D912 1
 pm2 -f start index.js -- v 0x50B2740e437410f30c2f679C06357eF1d76cedAE 1
 pm2 -f start index.js -- v 0x484D0e98f78550DBBCf95D49573F77B4Ab50a38C 1
 pm2 -f start index.js -- v 0xa186900e0e24C5a5943Ac10dF71B574debfFC74b 1
+
+node index.js
