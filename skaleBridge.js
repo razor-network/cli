1dbf7f83685bf567ff6347586b7417e170301b5b
Hrishikesh Huilgolkar <hrishikesh911@gmail.com>
2020-01-30 16:22:55 +0530

    update skalebridge to add name

--- skaleBridge.js
+++ skaleBridge.js
@@ -5,10 +5,10 @@ let bridgeBuild = require('./build/contracts/Bridge.json')
 const fs = require('fs')
 
 const provider = fs.readFileSync(".skale").toString().trim();
-
+let networkid = 1
 let web3 = new Web3(provider, null, {})
 let bridge = new web3.eth.Contract(bridgeBuild['abi'])
-
+console.log('contract address',bridgeBuild['networks'][1].address)
 // var Tx = require('ethereumjs-tx').Transaction
 // var ethjs = require('ethereumjs-util')
 
@@ -20,26 +20,52 @@ const privateKey = fs.readFileSync('.bridgeKey').toString().trim()
 web3.eth.accounts.wallet.add(privateKey)
 console.log('using address ', web3.eth.accounts.wallet[0].address)
 
-async function copy() {
-    let numJobs = await api.getNumJobs()
-    console.log('numJobs', numJobs)
+async function setResult(i) {
     let result
     let nonce
     let dataTx
-    for (let i = 1; i <= numJobs; i++) {
-        result = await api.getResult(i)
-        console.log('result', i, result)
-        dataTx = bridge.methods.setResult(Number(i), Number(result)).encodeABI()
-        nonce = await web3.eth.getTransactionCount(web3.eth.accounts.wallet[0].address, 'pending')
-        console.log('nonce', nonce)
+    result = await api.getResult(i)
+    console.log('result', i, result)
+    dataTx = bridge.methods.setResult(Number(i), Number(result)).encodeABI()
+    nonce = await web3.eth.getTransactionCount(web3.eth.accounts.wallet[0].address, 'pending')
+    console.log('nonce', nonce)
 
-        var rawTx = {
-            from: web3.eth.accounts.wallet[0].address,
-            to: bridgeBuild['networks'][1].address,
-            gas: 80000,
-            data: dataTx
-        }
-        await web3.eth.sendTransaction(rawTx)
+    var rawTx = {
+        from: web3.eth.accounts.wallet[0].address,
+        to: bridgeBuild['networks'][networkid].address,
+        gas: 80000,
+        data: dataTx
+    }
+    await web3.eth.sendTransaction(rawTx)
+}
+
+async function setJob(i) {
+    let job
+    let nonce
+    let dataTx
+    job = await api.getJob(i)
+    console.log('job', i, job)
+    console.log('setting', Number(i), job.url, job.selector, job.name, Number(job.result))
+    dataTx = await bridge.methods.setJob(Number(i), job.url, job.selector, job.name, Number(job.result)).encodeABI()
+    nonce = await web3.eth.getTransactionCount(web3.eth.accounts.wallet[0].address, 'pending')
+    console.log('nonce', nonce)
+
+    var rawTx = {
+        from: web3.eth.accounts.wallet[0].address,
+        to: bridgeBuild['networks'][networkid].address,
+        gas: 2000000,
+        data: dataTx
+    }
+    await web3.eth.sendTransaction(rawTx)
+}
+
+async function copy() {
+    let numJobs = await api.getNumJobs()
+    console.log('numJobs', numJobs)
+
+    for (let i = 1; i <= numJobs; i++) {
+        await setResult(i)
+        await setJob(i)
     }
     console.log('synced', Date())
 }
